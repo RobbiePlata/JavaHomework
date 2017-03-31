6ef8d2f66f0a8de3bd1e12a4077fe16f70979159
Robert Plata <norbertedguy@gmail.com>
2017-03-31 12:53:18 -0400

    Cleanup
    Lab 8 Quick and dirty vote program

---
+++ Lab 1 Print Statement/FirstAssignment.java
@@ -0,0 +1,19 @@
+package firstassignment;
+
+/**
+ *
+ * @author Robert Plata
+ */
+public class FirstAssignment {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Sergey Brin is a Soviet-born American computer scientist");
+        System.out.println("Brin earned his bachelor's degree at the University of Maryland");
+        System.out.println("Brin was the 12th richest person in the world with an estimated net worth of US$39.2 billion");
+        
+    }
+    
+}
